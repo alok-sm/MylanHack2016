1051867a26b7abf90fdbf9508d6026f21f2f143f
Alok <alok.shankar.m@gmail.com>
2016-03-20 12:01:30 +0530

    threat level

--- UI/js/shit.js
+++ UI/js/shit.js
@@ -1,10 +1,35 @@
 $(function(){
-    $("#country-input").select2();
-    $('#disease-input').select2({
-        placeholder: "Select a Disease"
+
+    function getColor(intensity){
+        return '#' + get2DigitHex(intensity) + get2DigitHex(100 - intensity) + '00'
+        // return 'red';
+    }
+
+    function get2DigitHex(intensity){
+        ret = Number(Math.floor(intensity/100 * 255)).toString(16)
+        if(ret.length == 1){
+            ret = '0' + ret;
+        }
+        return ret;
+    }
+
+    var percent_number_step = $.animateNumber.numberStepFactories.append(' %')
+    $('#fun-level').animateNumber(
+        {
+            number: 25,
+            color: getColor(50),
+            easing: 'easeInQuad',
+            numberStep: percent_number_step
+        },
+        10000
+    );
+    // $("#fun-level").animate({ color: "blue" }, 10000);
+
+    $('#country-input').select2({
+        placeholder: 'Select Country'
     });
     $('#state-input').select2({
-        placeholder: "Select a State"
+        placeholder: 'Select State'
     });
     var sltext = '<option value="Bm">Bombali District</option><option value="Ko">Koinadugu District</option><option value="Po">Port Loko District</option><option value="To">Tonkolili District</option><option value="Ka">Kambia District</option><option value="Ke">Kenema District</option><option value="Kn">Kono District</option><option value="Kl">Kailahun District</option><option value="Bo">Bo District</option><option value="Bn">Bonthe District</option><option value="Pu">Pujehun District</option><option value="Mo">Moyamba District</option><option value="Wu">Western Area Urban District</option><option value="Wr">Western Area Rural District</option>'
     var intext = '<option value="Up">Uttar Pradesh</option><option value="Mh">Maharashtra</option><option value="Bi">Bihar</option><option value="Wb">West Bengal</option><option value="Mp">Madhya Pradesh</option><option value="Tn">Tamil Nadu</option><option value="Rj">Rajasthan</option><option value="Ka">Karnataka</option><option value="Gj">Gujarat</option><option value="Ap">Andhra Pradesh</option><option value="Od">Odisha</option><option value="Ts">Telangana</option><option value="Kl">Kerala</option><option value="Jk">Jharkhand</option><option value="As">Assam</option><option value="Pn">Punjab</option><option value="Ch">Chhattisgarh</option><option value="Hr">Haryana</option><option value="Jk">Jammu and Kashmir</option><option value="Uk">Uttarakhand</option><option value="Hp">Himachal Pradesh</option><option value="Tr">Tripura</option><option value="Mg">Meghalaya</option><option value="Mn">Manipur</option><option value="Ng">Nagaland</option><option value="Ga">Goa</option><option value="Ar">Arunachal Pradesh</option><option value="Mz">Mizoram</option><option value="Sk">Sikkim</option><option value="Dl">National Capital Region</option>'
